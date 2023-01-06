c551f33e3ead3c0a166da27e048c78a830291eb0
Beanxx <tnqls991211@naver.com>
2023-01-06 16:44:00 +0900

    feat: Todo style 추가

---
+++ client/components/Todo/TodoEditor/TodoEditor.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Layout = styled.form`
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  
+  .input_box {
+    width: 400px;
+  }
+
+  .btn_box {
+    width: 400px;
+  }
+`
---
+++ client/components/Todo/TodoList/TodoList.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  width: 45%;
+  /* height: 500px; */
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  /* overflow-y: scroll; */
+  /* background-color: aliceblue; */
+`
