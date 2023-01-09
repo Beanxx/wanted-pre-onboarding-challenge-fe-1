08636ffbe386237ec1b1112bea8a7ae86fe7992a
Beanxx <tnqls991211@naver.com>
2023-01-10 00:42:51 +0900

    feat: install moment & 날짜 포맷 변경

--- client/components/Todo/TodoEditor/TodoEditor.tsx
+++ client/components/Todo/TodoEditor/TodoEditor.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import * as S from "./TodoEditor.styles";
 import { TodoType } from "../../../types/todo.interface";
 import { Btn } from "../../UI/Button/Button";
+import moment from "moment";
+import "moment/locale/ko";
 
 const TodoEditor: React.FC<{
   data: TodoType[];
@@ -11,12 +13,15 @@ const TodoEditor: React.FC<{
     deleteTodoHanlder(data[0]?.id);
   };
 
+  const createdAt = moment(data[0]?.createdAt).format("YYYY년 MMMM Do, a h:mm");
+  const updatedAt = moment(data[0]?.updatedAt).format("YYYY년 MMMM Do, a h:mm");
+
   return (
     <S.Layout>
       <div>{data[0]?.title}</div>
       <div>{data[0]?.content}</div>
-      <div>{data[0]?.createdAt.toString()}</div>
-      <div>{data[0]?.updatedAt}</div>
+      <div>{createdAt}</div>
+      <div>{updatedAt}</div>
       <Btn type="button" onClick={onDelete}>
         Delete
       </Btn>
