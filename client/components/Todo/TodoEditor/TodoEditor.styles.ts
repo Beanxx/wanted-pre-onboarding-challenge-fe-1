fe18f3e2d8336044b4eaa6f6403b31324ca457b2
Beanxx <tnqls991211@naver.com>
2023-01-10 01:22:36 +0900

    design: Todo Detail Content style 수정

--- client/components/Todo/TodoEditor/TodoEditor.styles.ts
+++ client/components/Todo/TodoEditor/TodoEditor.styles.ts
@@ -1,10 +1,33 @@
 import styled from "styled-components"
 
 export const Layout = styled.form`
-  width: 90%;
+  width: 100%;
+  height: 300px;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 1rem;
-  border: 1px solid aliceblue;
+`
+
+
+export const Box = styled.div`
+  width: 90%; 
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
+
+export const Label = styled.div`
+  font-size: 15px;
+  margin-bottom: 5px;
+`
+
+export const Output = styled.div`
+  width: 100%;
+  background-color: var(--side-bgc);
+  border-radius: 7px;
+  padding: 10px;
+  border: 1px solid var(--border-color);
+  text-align: left;
+  font-size: 13px;
 `
