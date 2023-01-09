eb9afcca60556f140dc62d910235f8d93d226efa
Beanxx <tnqls991211@naver.com>
2023-01-09 23:02:47 +0900

    chore: move api folder

--- client/apis/api.ts
+++ client/apis/api.ts
@@ -10,11 +10,36 @@ export const getTodos = () => {
     })
 };
 
+
+export const getTodo = (id: string) => {
+  return axios
+    .get(`http://localhost:8080/todos/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${localStorage.getItem("Token")}`,
+      },
+    })
+};
+
+
 export const postTodo = (title: string, content: string) => {
   return axios
     .post(
       `http://localhost:8080/todos`,
       { title, content },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${localStorage.getItem("Token")}`,
+        },
+      }
+    )
+};
+
+export const deleteTodo = (id: string) => {
+  return axios
+    .delete(
+      `http://localhost:8080/todos/${id}`,
       {
         headers: {
           "Content-Type": "application/json",
