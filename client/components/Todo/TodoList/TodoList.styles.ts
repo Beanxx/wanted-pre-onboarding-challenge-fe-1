9adcb6638df91a6a905f3f7af5c832e205c66eb9
Beanxx <tnqls991211@naver.com>
2023-01-10 00:12:51 +0900

    chore: remove comment

--- client/components/Todo/TodoList/TodoList.styles.ts
+++ client/components/Todo/TodoList/TodoList.styles.ts
@@ -1,14 +1,11 @@
 import styled from "styled-components";
 
 export const Layout = styled.div`
-  width: 45%;
-  /* height: 500px; */
+  width: 100%;
   padding: 10px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 10px;
-  /* overflow-y: scroll; */
-  /* background-color: aliceblue; */
 `
