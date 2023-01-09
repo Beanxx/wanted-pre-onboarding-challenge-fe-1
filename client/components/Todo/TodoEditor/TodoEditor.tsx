0f92f44c3a7c3caa49aee2262c735af25622a4fe
Beanxx <tnqls991211@naver.com>
2023-01-10 05:08:15 +0900

    feat: Todo - update 기능 구현

--- client/components/Todo/TodoEditor/TodoEditor.tsx
+++ client/components/Todo/TodoEditor/TodoEditor.tsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./TodoEditor.styles";
 import { TodoType } from "../../../types/todo.interface";
 import { Btn } from "../../UI/Button/Button";
 import moment from "moment";
 import "moment/locale/ko";
+import Input from "../../UI/Input/Input";
 
-const TodoEditor: React.FC<{
+interface Props {
   data: TodoType[];
   deleteTodoHanlder: (id: string) => void;
-}> = ({ data, deleteTodoHanlder }) => {
+  updateTodoHanlder: (id: string, title: string, content: string) => void;
+}
+
+const TodoEditor: React.FC<Props> = ({
+  data,
+  deleteTodoHanlder,
+  updateTodoHanlder,
+}) => {
+  const [editTitle, setEditTitle] = useState(data[0]?.title);
+  const [editContent, setEditContent] = useState(data[0]?.content);
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEditMode = () => {
+    setIsEdit(true);
+  };
+
+  const cancelEditMode = () => {
+    setIsEdit(false);
+    setEditTitle(data[0]?.title);
+    setEditContent(data[0]?.content);
+  };
+
+  const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditTitle(e.target.value);
+  };
+
+  const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditContent(e.target.value);
+  };
+
   const setDate = (a: string, b: string) => {
     if (a !== b) {
       return `${moment(b).format("YYYY년 MMMM Do, HH:mm")} (수정됨)`;
@@ -21,11 +52,26 @@ const TodoEditor: React.FC<{
     deleteTodoHanlder(data[0]?.id);
   };
 
+  const onUpdate = () => {
+    updateTodoHanlder(data[0]?.id, editTitle, editContent);
+    setIsEdit(false);
+  };
+
   return (
     <S.Layout>
       <S.Box>
         <S.Label>Title</S.Label>
-        <S.Output>{data[0]?.title}</S.Output>
+
+        {isEdit ? (
+          <Input
+            id="title"
+            type="text"
+            value={editTitle}
+            onChange={onChangeTitle}
+          />
+        ) : (
+          <S.Output>{data[0]?.title}</S.Output>
+        )}
       </S.Box>
       <S.Box>
         <S.Label>Date</S.Label>
@@ -33,12 +79,47 @@ const TodoEditor: React.FC<{
       </S.Box>
       <S.Box>
         <S.Label>Content</S.Label>
-        <S.Output>{data[0]?.content}</S.Output>
+        {isEdit ? (
+          <Input
+            id="content"
+            type="text"
+            value={editContent}
+            onChange={onChangeContent}
+          />
+        ) : (
+          <S.Output>{data[0]?.content}</S.Output>
+        )}
       </S.Box>
 
-      <Btn type="button" onClick={onDelete}>
-        Delete
-      </Btn>
+      <S.BtnBox>
+        {isEdit ? (
+          <>
+            <div>
+              <Btn type="button" onClick={cancelEditMode}>
+                Cancel
+              </Btn>
+            </div>
+            <div>
+              <Btn type="button" onClick={onUpdate}>
+                Update
+              </Btn>
+            </div>
+          </>
+        ) : (
+          <>
+            <div>
+              <Btn type="button" onClick={onDelete}>
+                Delete
+              </Btn>
+            </div>
+            <div>
+              <Btn type="button" onClick={handleEditMode}>
+                Edit
+              </Btn>
+            </div>
+          </>
+        )}
+      </S.BtnBox>
     </S.Layout>
   );
 };
--- client/pages/todo/index.tsx
+++ client/pages/todo/index.tsx
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import TodoPost from "../../components/Todo/TodoPost/TodoPost";
 import TodoList from "../../components/Todo/TodoList/TodoList";
-import { getTodos, getTodo, postTodo, deleteTodo } from "../../apis/api";
+import {
+  getTodos,
+  getTodo,
+  postTodo,
+  deleteTodo,
+  updateTodo,
+} from "../../apis/api";
 import TodoEditor from "../../components/Todo/TodoEditor/TodoEditor";
 import { TodoType } from "../../types/todo.interface";
 import { useRouter } from "next/router";
+
 const index = () => {
   const router = useRouter();
 
@@ -52,20 +59,38 @@ const index = () => {
       });
   };
 
+  const updateTodoHanlder = async (
+    id: string,
+    title: string,
+    content: string
+  ) => {
+    await updateTodo(id, title, content)
+      .then((res) => {
+        getTodosHandler();
+        getTodoHandler(id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     getTodosHandler();
   }, []);
 
   return (
     <Layout>
-      <h1>Todo</h1>
+      <h1>Todo List</h1>
       <Container>
         <div className="left_box">
           <TodoPost postTodoHandler={postTodoHandler} />
-          {detailData.length !== 0 && (
+          {detailData.length === 0 ? (
+            <EmptyBox />
+          ) : (
             <TodoEditor
               deleteTodoHanlder={deleteTodoHanlder}
               data={detailData}
+              updateTodoHanlder={updateTodoHanlder}
             />
           )}
         </div>
@@ -107,7 +132,11 @@ const Container = styled.div`
     width: 400px;
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
   }
+`;
+
+const EmptyBox = styled.div`
+  width: 90%;
+  height: 300px;
 `;
